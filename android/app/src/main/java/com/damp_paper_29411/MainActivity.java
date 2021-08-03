d7687aedb7c109859dc67c1db9464e4378c058b7
Crowdbotics <team@crowdbotics.com>
2021-08-03 18:14:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/damp_paper_29411/MainActivity.java
@@ -0,0 +1,15 @@
+package com.damp_paper_29411;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "damp_paper_29411";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_paper_29411Identifier",
+  url: "https://damp_paper_29411Identifier.botics.co",
+  api: "https://damp_paper_29411Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
